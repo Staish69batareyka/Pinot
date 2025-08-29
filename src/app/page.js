8b7b99104dcbb1579dbaa755d263dcef32caafb5
Staish69Batareyka <anastaish.batareyka@yandex.ru>
2025-08-29 17:18:43 +0300

    STYLE: базовая разметка и общие стили

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,21 @@ export default function Page() {
 
     return (
         <>
-            <div>
-
+            <div className={styles.container}>
+                <div className={`${styles.side} ${styles.left}`}>
+                    <div className={styles.content}>
+                        <h1>left section</h1>
+                    </div>
+                </div>
+                <div className={`${styles.side} ${styles.right}`}>
+                    <div className={styles.content}>
+                        <h1>right section</h1>
+                    </div>
+                </div>
+                <div
+                    className={`${styles.logo}`}>
+                    <img src='' alt="logo" />
+                </div>
             </div>
         </>
     );
