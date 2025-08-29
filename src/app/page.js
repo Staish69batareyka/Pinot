3d5467d329cfd86cb60ad027f3f5a038bc9b683d
Staish69Batareyka <anastaish.batareyka@yandex.ru>
2025-08-29 17:43:15 +0300

    STYLE: сделала по возможности похожий стиль

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,39 @@
 "use client";
-import styles from "./globals.css";
+import {useState} from "react";
+import styles from './page.module.css'
 
 export default function Page() {
+
+    const [hover, setHover] = useState(null);
 
     return (
         <>
             <div className={styles.container}>
-                <div className={`${styles.side} ${styles.left}`}>
+                <div className={`${styles.side} ${styles.left}
+                ${hover === 'left' ? styles.active : 
+                    hover === 'right' ? styles.inactive : ""}`}
+                     onMouseLeave={() => setHover(null)}
+                     onMouseEnter={() => {setHover('left')}}>
+
                     <div className={styles.content}>
-                        <h1>left section</h1>
+                        <h1>Туристический комплекс</h1>
                     </div>
+
                 </div>
-                <div className={`${styles.side} ${styles.right}`}>
+                <div className={`${styles.side} ${styles.right}
+                ${hover === 'right' ? styles.active :
+                hover === 'left' ? styles.inactive : ''}`}
+                onMouseEnter={() => setHover('right')}
+                onMouseLeave={() => setHover(null)}>
                     <div className={styles.content}>
-                        <h1>right section</h1>
+                        <h1>Гравитационная винодельня</h1>
                     </div>
                 </div>
                 <div
-                    className={`${styles.logo}`}>
-                    <img src='' alt="logo" />
+                    className={`${styles.logo} 
+                    ${hover === 'left' ? styles.logoRight : 
+                    hover === 'right' ? styles.logoLeft : ''}`}>
+                    <img src='favicon.ico' alt="logo" />
                 </div>
             </div>
         </>
